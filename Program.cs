886cdccc26ba9509141a8b0841cd82ff612ef1f3
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-08-31 20:09:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        class Calculator
+        {
+
+            public int addition(params int[] lst)
+            {
+                int total = 0;
+                foreach (int num in lst)
+                {
+
+                    total += num;
+                }
+                return total;
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Calculator Demo");
+                Calculator calc = new Calculator();
+                Console.WriteLine(calc.addition(10, 20, 30));
+                Console.WriteLine(calc.addition(23, 16));
+                Console.WriteLine(calc.addition(12, 40));
+                
+            }
+        }
+    }
+}
